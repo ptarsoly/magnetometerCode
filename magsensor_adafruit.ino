7d6d8cc87f9c0eb5aaf776a30d7714721bb7c2fc
ptarsoly <ptarsoly@users.noreply.github.com>
2017-03-30 15:59:46 -0400

    Prints out calibrated values

--- magsensor_adafruit.ino
+++ magsensor_adafruit.ino
@@ -37,6 +37,19 @@
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 
+double val1;
+double val1Min; 
+double val1Max;
+double val2;
+double val2Min; 
+double val2Max;
+double val3;
+double val3Min; 
+double val3Max;
+
+int calibrationPin = 13;
+int lad = HIGH;
+
 void displaySensorDetails(void)
 {
   sensor_t sensor;
@@ -57,6 +70,10 @@ void setup(void)
 {
   Serial.begin(9600);
   Serial.println("HMC5883 Magnetometer Test"); Serial.println("");
+
+  pinMode(calibrationPin,OUTPUT);
+  sensors_event_t event; 
+  mag.getEvent(&event);
   
   /* Initialise the sensor */
   if(!mag.begin())
@@ -65,6 +82,10 @@ void setup(void)
     Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
     while(1);
   }
+
+  
+  digitalWrite(calibrationPin, LOW);  
+  
   
   /* Display some basic information on this sensor */
   displaySensorDetails();
@@ -77,37 +98,9 @@ void loop(void)
   mag.getEvent(&event);
  
   /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
-  Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
-
-  //the following is for a compass heading. For what we are working with,
-  //this function will not be needed.
-
-/*
-  // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
-  // Calculate heading when the magnetometer is level, then correct for signs of axis.
-  float heading = atan2(event.magnetic.y, event.magnetic.x);
-  
-  // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
-  // Find yours here: http://www.magnetic-declination.com/
-  // Mine is: -13* 2' W, which is ~13 Degrees, or (which we need) 0.22 radians
-  // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
-  float declinationAngle = 0.22;
-  heading += declinationAngle;
-  
-  // Correct for when signs are reversed.
-  if(heading < 0)
-    heading += 2*PI;
-    
-  // Check for wrap due to addition of declination.
-  if(heading > 2*PI)
-    heading -= 2*PI;
-   
-  // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180/M_PI; 
-  
-  Serial.print("Heading (degrees): "); Serial.println(headingDegrees);*/
+  Serial.print("X: "); Serial.print(/*map(abs(*/6+event.magnetic.x/*),val1Min,val1Max,0,55))*/); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(/*map(abs(*/8+event.magnetic.y/*),val2Min,val2Max,0,55))*/); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(/*map(abs(*/12+event.magnetic.z/*),val3Min,val3Max,0,55))*/); Serial.print("  ");Serial.println("uT");
   
   delay(250);
 }
